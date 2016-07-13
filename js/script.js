63aeadd1b98c88c592a6b5c14ee28bb9c2384d15
Toan Nguyen <n.minhtoan@gmail.com>
2016-07-13 22:58:38 +0700

    add map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 /**
- * Created by tmblue on 7/11/16.
+ *
  */
+
+(function($){
+	var map = null;
+	$(document).ready(function(){
+		// map
+		console.log('abc');
+		map = new GMaps({
+			div: "#map",
+			lat: -12.043333,
+			lng: -77.028333,
+   			scrollwheel: false,
+		});
+	});
+
+})(jQuery);
