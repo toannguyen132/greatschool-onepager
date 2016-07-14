d734b52be5fdc6d56759292768f00a2e91765964
Toan Nguyen <n.minhtoan@gmail.com>
2016-07-14 16:27:24 +0700

    Update tooltips

--- js/script.js
+++ js/script.js
@@ -6,13 +6,33 @@
 	var map = null;
 	$(document).ready(function(){
 		// map
-		console.log('abc');
 		map = new GMaps({
 			div: "#map",
 			lat: -12.043333,
 			lng: -77.028333,
    			scrollwheel: false,
 		});
+
+		// tootip
+		$('.tlp').tooltipster({
+			maxWidth: 300,
+			debug: false
+		});
+
+		// sidebar
+		function sidebar_context(){
+			$context = $('#context_hint');
+			offset = $context.offset().top;
+			anchor = offset + $context.outerHeight();
+			// console.log(offset);
+			$(window).scroll(function(){
+				var top = $('body').scrollTop();
+				if ( top > anchor ){
+					console.log('over');
+				}
+			});	
+		}
+		sidebar_context();
 	});
 
 })(jQuery);
