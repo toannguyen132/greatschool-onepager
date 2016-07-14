ee06d007460e8ce8d14fc903d55cf493024e306e
Toan Nguyen <n.minhtoan@gmail.com>
2016-07-14 18:46:12 +0700

    Update sidebar

--- js/script.js
+++ js/script.js
@@ -19,16 +19,36 @@
 			debug: false
 		});
 
+		var isMobile = $(window).width() <= 768;
+
 		// sidebar
 		function sidebar_context(){
 			$context = $('#context_hint');
 			offset = $context.offset().top;
 			anchor = offset + $context.outerHeight();
+
+			$context.css('width', $context.width() ); // 30 is padding
 			// console.log(offset);
 			$(window).scroll(function(){
+				if ( isMobile ) return;
+
 				var top = $('body').scrollTop();
+				var pos_left = $context.parent().offset().left + 15; // 15 is padding
+				var pos_top = 30;
 				if ( top > anchor ){
-					console.log('over');
+					$context.removeClass('hided');
+					$context.css({
+						"position": 'fixed',
+						"top": pos_top,
+						"left": pos_left
+					});
+				} else{
+					$context.addClass('hided');
+					$context.css({
+						"position": 'relative',
+						"top": 'auto',
+						"left": 'auto'
+					});
 				}
 			});	
 		}
